e224ad82eb3369bd97c6c93720ddae2f1df99b94
Fabiano Bizarro <fabianoasbizarro@gmail.com>
2021-07-19 23:15:31 -0300

    add files

---
+++ lexer/lexer.go
@@ -0,0 +1,105 @@
+package lexer
+
+import "monkey/token"
+
+type Lexer struct {
+	input        string
+	position     int
+	readPosition int
+	ch           byte
+}
+
+func New(input string) *Lexer {
+	lexer := &Lexer{input: input}
+	lexer.readChar()
+	return lexer
+}
+
+func (l *Lexer) nextToken() token.Token {
+	var tk token.Token
+
+	l.skipWhitespace()
+
+	switch l.ch {
+	case '=':
+		tk = newToken(token.ASSIGN, l.ch)
+	case '(':
+		tk = newToken(token.LPAREN, l.ch)
+	case ')':
+		tk = newToken(token.RPAREN, l.ch)
+	case ';':
+		tk = newToken(token.SEMICOLON, l.ch)
+	case ',':
+		tk = newToken(token.COMMA, l.ch)
+	case '+':
+		tk = newToken(token.PLUS, l.ch)
+	case '{':
+		tk = newToken(token.LBRACE, l.ch)
+	case '}':
+		tk = newToken(token.RBRACE, l.ch)
+	case 0:
+		tk.Literal = ""
+		tk.Type = token.EOF
+	default:
+		if isLetter(l.ch) {
+			tk.Literal = l.readIdentifier()
+			tk.Type = token.LookupIdent(tk.Literal)
+			return tk
+		} else if isDigit(l.ch) {
+			tk.Type = token.INT
+			tk.Literal = l.readNumber()
+			return tk
+		} else {
+			tk = newToken(token.ILLEGAL, l.ch)
+		}
+	}
+
+	l.readChar()
+	return tk
+}
+
+func (l *Lexer) readChar() {
+	if l.readPosition >= len(l.input) {
+		l.ch = 0
+	} else {
+		l.ch = l.input[l.readPosition]
+	}
+
+	l.position = l.readPosition
+	l.readPosition += 1
+}
+
+func (l *Lexer) readIdentifier() string {
+	position := l.position
+	for isLetter(l.ch) {
+		l.readChar()
+	}
+
+	return l.input[position:l.position]
+}
+
+func (l *Lexer) readNumber() string {
+	position := l.position
+	for isDigit(l.ch) {
+		l.readChar()
+	}
+
+	return l.input[position:l.position]
+}
+
+func (lexer *Lexer) skipWhitespace() {
+	for lexer.ch == ' ' || lexer.ch == '\t' || lexer.ch == '\r' || lexer.ch == '\n' {
+		lexer.readChar()
+	}
+}
+
+func newToken(tokenType token.TokenType, ch byte) token.Token {
+	return token.Token{Type: tokenType, Literal: string(ch)}
+}
+
+func isLetter(ch byte) bool {
+	return 'a' <= ch && ch <= 'z' || 'A' <= ch && ch <= 'Z' || ch == '_'
+}
+func isDigit(ch byte) bool {
+	return '0' <= ch && ch <= '9'
+}
